9308abf72c67e90146179e9848c225314951c486
sangki-song <126885150+nullpointer3202@users.noreply.github.com>
2026-05-28 11:03:56 +0900

    latin1_to_hangul.py 업데이트
    euc-kr와 cp949 변환 코드 통합, 오류 언더바와 원본 언더바 구분 과정 추가

--- latin1_to_hangul.py
+++ latin1_to_hangul.py
@@ -1,24 +1,32 @@
-def decode_korean(s):
-    raw = s.encode("latin-1")
+def latin_to_cp949(txt) :
+    raw = txt.encode("latin-1")
+    raw_patched = bytearray();
+    is_fair = 0
 
-    # 1차: euc-kr 시도
-    try:
-        return raw.decode("euc-kr"), "euc-kr"
-    except UnicodeDecodeError:
-        pass
+    for idx, b in enumerate (raw):
+        if (chr(b).isascii()) :         #   바이트가 아스키문자라면
+            if (is_fair == 1 and b == 0x5F) :   # 한글 바이트 끝부분이면서 _ 문자일경우
+                raw_patched.append(0xAD)
+                is_fair = 0
+                continue
 
-    # 2차: cp949 시도 (0x5F → 0xAD 치환)
-    raw_patched = bytes([0xAD if b == 0x5F else b for b in raw])
-    try:
-        return raw_patched.decode("cp949"), "cp949 (0x5F -> 0xAD)"
-    except UnicodeDecodeError:
-        pass
+            raw_patched.append(b)
+            is_fair = 0
+            continue
+        else :
+            if (is_fair == 0) : #한글 바이트 시작점
+                raw_patched.append(b)
+                is_fair = 1
+                continue
+            else :  #한글 바이트 끝
+                raw_patched.append(b)
+                is_fair = 0
+            
 
-    # 3차: 치환 없이 cp949 시도
-    return raw.decode("cp949", errors="replace"), "cp949 (w/ replace)"
+    return raw_patched.decode("cp949", errors="replace"), "cp949"
 
 
 
 s = input()
-result, enc = decode_korean(s)
+result, enc = latin_to_cp949(s)
 print(f"[{enc}] {result}")
